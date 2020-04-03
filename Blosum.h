16b4a0c4d9ec96b1135dc9dfe832df3409d718da
JCNdox <57673610+JCNdox@users.noreply.github.com>
2020-04-03 15:22:30 +0200

    Add files via upload

---
+++ Blosum.h
@@ -0,0 +1,34 @@
+/*
+ La classe fonctionne. Il reste à définir la fonction score
+*/
+
+#ifndef BLOSUM_H_INCLUDED
+#define BLOSUM_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <sstream>
+
+class Blosum
+{
+    public:
+    Blosum(std::string fileName); //constructeur
+    Blosum(Blosum const& original); // constructeur par copie
+    ~Blosum(); //destructeur
+
+    int score(char a, char b) const;
+    void init(std::string fileName);
+    int preScore(char c) const; //changer ce nom
+    std::vector<int> lecture(std::string ligne);
+
+    private:
+    int m_matrice[24][24];
+    std::string m_fileName;
+
+};
+
+
+#endif // BLOSUM_H_INCLUDED
