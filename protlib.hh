227bbe6682511a8ac1a2194b10fbb779a1e0b23e
JCNdox <57673610+JCNdox@users.noreply.github.com>
2020-04-03 18:11:36 +0200

    Delete protlib.hh

--- protlib.hh
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <cstdio>
-int bigToLittle(unsigned char buffer[], int n);
-unsigned int readIndexFile(unsigned char * buffer, char sizeOfBuffer, FILE * indexFile);
-std::string charToString(char* a, int size);
-std::string blastHeaderDecode(char stringBuffer[]);
